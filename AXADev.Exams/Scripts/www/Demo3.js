33c4cb21a488449ee6bf05a4ff114bc2213b5ba8
Mark John Rodriguez <mark.rodriguez@mobilegroupinc.com>
2020-06-29 11:58:54 +0800

    home design

--- AXADev.Exams/Scripts/www/Demo3.js
+++ AXADev.Exams/Scripts/www/Demo3.js
@@ -10,25 +10,5 @@
         });
     });
 
-    $(document).on('submit', '#frmResumeSend', ResumeSendSubmit);
 });
 
-function ResumeSendSubmit() {
-    var file = $("#file").val();
-    if (file === "") {
-        alert("Please select a file first to proceed");
-        return false;
-    }
-    return true;
-}
-
-
-function checkuploadfile(me) {
-    var ext = $(me).val().split('.').pop().toLowerCase();
-
-    if (($.inArray(ext, ['pdf']) == -1) && ($(me).val() != "")) {
-        $(me).val(null);
-        alert("File must be a PDF document.");
-    }
-
-}
